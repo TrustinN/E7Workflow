39ddf38eb215b560765d4a43703542cdab0ad52a
TrustinN <trustin.nguyen62@berkeley.edu>
2026-06-24 21:15:16 -0700

    runner works

--- src/app/components/runner/main.py
+++ src/app/components/runner/main.py
@@ -39,7 +39,6 @@ class RunnerComponent(Node):
         link = Link(ActionRoute.NAME, ActionRoute.CREATE)
         resp = self.client.post(link)
         self.model.setAction(selection.id, resp["id"])
-        print(self.model.toData())
 
     def setScript(self):
         selection = self.context.selectionModel.getSelected()
@@ -48,14 +47,12 @@ class RunnerComponent(Node):
         link = Link(ScriptRoute.NAME, ScriptRoute.SCRIPT)
         resp = self.client.get(link)
         self.model.setScript(selection.id, resp["id"])
-        print(self.model.toData())
 
     def setEntry(self):
         selection = self.context.selectionModel.getSelected()
         if not (selection.id and selection.type == SelectionType.WORKSPACE):
             return
         self.model.setEntry(selection.id)
-        print(self.model.toData())
 
     def saveState(self, data):
         path = data["path"]
